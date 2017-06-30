ae0ebde9456deaec99b9ce49c6e175360da762b6
marceloroliveira81 <marceloroliveira81@gmail.com>
2017-06-30 12:05:20 -0500

    Modificação do controller.js
    Inclusão de objetos va variável listaDeItens
    
    Atribuição a variável itemSelecionado (Json) o objeto passado por
    parâmentro

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,19 +1,19 @@
 angular.module('starter')
 .controller('controllerLista', function($scope){
 	$scope.listaDeItens = [
-		'Item 1',
-		'Item 2',
-		'Item 3',
-		'Item 4',
-		'Item 5',
-		'Item 6',
-		'Item 7',
-		'Item 9',
-		'Item 10'
+		{nome: 'Item 1', valor: '100', detalhe: {descricao: 'Descrição Item 1'}},
+		{nome: 'Item 2', valor: '1000', detalhe: {descricao: 'Descrição Item 2'}},
+		{nome: 'Item 3', valor: '200.57', detalhe: {descricao: 'Descrição Item 3'}},
+		{nome: 'Item 4', valor: '300', detalhe: {descricao: 'Descrição Item 4'}},
+		{nome: 'Item 5', valor: '250', detalhe: {descricao: 'Descrição Item 5'}},
+		{nome: 'Item 6', valor: '199', detalhe: {descricao: 'Descrição Item 6'}},
+		{nome: 'Item 7', valor: '460', detalhe: {descricao: 'Descrição Item 7'}},
+		{nome: 'Item 8', valor: '230', detalhe: {descricao: 'Descrição Item 8'}},
+		{nome: 'Item 9', valor: '99.87', detalhe: {descricao: 'Descrição Item 9'}},
 	];
 });
 
 angular.module('starter')
 .controller('controllerItemSelecionado', function($stateParams, $scope){
-	$scope.itemSelecionado = $stateParams.item;
+	$scope.itemSelecionado =  angular.fromJson($stateParams.item);
 });
